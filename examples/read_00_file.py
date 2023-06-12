e4b57bc7b491223646ab1832206877f76c36106f
Robert Jackson <rjackson@Roberts-MacBook-Pro-3.local>
2023-06-12 11:53:30 -0500

    STY: Pep8 fixes

--- examples/read_00_file.py
+++ examples/read_00_file.py
@@ -1,3 +1,8 @@
+"""
+
+Example on reading raw Halo Photonics data
+------------------------------------------
+"""
 import highiq
 
 ds = highiq.io.read_00_data('sgpdlacfC1.00.20220107.141001.raw.aet_Stare_107_20220107_14.raw',
